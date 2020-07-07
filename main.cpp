22de174ba1bc31008becad4b794bef11d7504cc6
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-07 13:09:22 -0400

    Update main.cpp
    This one if final one and the best one. Used [] operators from unordered map. Earlier I was using find function like any other map. I totally missed [] operator from unordered map.

--- main.cpp
+++ main.cpp
@@ -5,25 +5,8 @@ public:
         string res = "";
         
         for (int i = 0; i < paths.size(); ++i) {
-            unordered_map<string, int>::iterator iter = tempMap.find(paths[i][0]);
-            unordered_map<string, int>::iterator endIter = tempMap.end();
-            
-            if (iter != endIter) {
-                iter->second += 1;
-            }
-            else {
-                tempMap.insert(pair<string, int>(paths[i][0], 1));
-            }
-            
-            iter = tempMap.find(paths[i][1]);
-            endIter = tempMap.end();
-            
-            if (iter != endIter) {
-                iter->second += 2;
-            }
-            else {
-                tempMap.insert(pair<string, int>(paths[i][1], 2));
-            }
+            tempMap[paths[i][0]] += 1;
+            tempMap[paths[i][1]] += 2;
         }
         
         unordered_map<string, int>::iterator iter = tempMap.begin();
