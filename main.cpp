eccb1f975a2b59e2f0db642e05518eea37479e25
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-07 13:01:23 -0400

    Update main.cpp
    Using unordered map and got much better results.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        map<string, int> tempMap;
+        unordered_map<string, int> tempMap;
         string res = "";
         
         for (int i = 0; i < paths.size(); ++i) {
-            map<string, int>::iterator iter = tempMap.find(paths[i][0]);
-            map<string, int>::iterator endIter = tempMap.end();
+            unordered_map<string, int>::iterator iter = tempMap.find(paths[i][0]);
+            unordered_map<string, int>::iterator endIter = tempMap.end();
             
             if (iter != endIter) {
                 iter->second += 1;
@@ -26,8 +26,8 @@ public:
             }
         }
         
-        map<string, int>::iterator iter = tempMap.begin();
-        map<string, int>::iterator endIter = tempMap.end();
+        unordered_map<string, int>::iterator iter = tempMap.begin();
+        unordered_map<string, int>::iterator endIter = tempMap.end();
         
         for (; iter != endIter; ++iter) {
             if (iter->second == 2) {
